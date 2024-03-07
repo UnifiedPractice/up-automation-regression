99d1e86547444a8d1af85403c224f9d58c2faa0d
alexandrucristian15 <87300455+alexandrucristian15@users.noreply.github.com>
2024-03-07 18:57:46 +0200

    Update patient-portal.ts

--- cypress/e2e/PageObject/patient-portal.ts
+++ cypress/e2e/PageObject/patient-portal.ts
@@ -944,7 +944,7 @@ class PatientPortal extends BasePage {
             const checkCompleteFormsExist= $box.text().includes('Complete Forms');
 
             if (checkSaveButtonExist || checkCompleteFormsExist) {
-                cy.wait(6000).get(".btn.btn-primary").click({force:true}).wait(8500)
+                cy.wait(26000).get(".btn.btn-primary").click({force:true}).wait(28500)
                     .then(() => this.checkFinalStepForms())
             }
             })
