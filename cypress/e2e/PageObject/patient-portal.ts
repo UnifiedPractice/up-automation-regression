0ba023bec11adef90e8591e64db59aef0a0c0a6d
alexandrucristian15 <87300455+alexandrucristian15@users.noreply.github.com>
2024-04-03 12:42:46 +0300

    Patient Portal Regression - 03.04.2024 - 2

--- cypress/e2e/PageObject/patient-portal.ts
+++ cypress/e2e/PageObject/patient-portal.ts
@@ -48,7 +48,7 @@ class PatientPortal extends BasePage {
     public bookApointmentSelector : string = '.text-right';
     private uploadLiveChatSelector: string = '.rfu-file-upload-button';
     private secondSelectBoxSelector
-    private usernameLogin: string= 'automation18@email.com';
+    private usernameLogin: string= 'automation19@email.com';
     private passwordLogin: string = 'password';
     private cancelButton: string = '.mat-icon.notranslate.mt-1.cancel-icon.mat-icon-no-color';
     private rescheduleButoon: string = '.mat-icon.notranslate.mr-1.material-icons.mat-icon-no-color';
@@ -134,7 +134,7 @@ class PatientPortal extends BasePage {
     }
 
     proceedLogin() : void {
-        cy.wait(5000).get('.inp').eq(0).click().type('automation18@email.com');
+        cy.wait(5000).get('.inp').eq(0).click().type('automation19@email.com');
         cy.contains('Password').parent().click().type('password');
         cy.get(this.loginButtonSelector).click().wait(19000);
     }
@@ -791,7 +791,7 @@ class PatientPortal extends BasePage {
 
     forgotPassword() : void{
         cy.contains('Forgot password?').click();
-        cy.get(this.forgotFieldSelector).click().type('automation18@email.com')
+        cy.get(this.forgotFieldSelector).click().type('automation19@email.com')
         cy.contains('Send Me Instructions').click();
         cy.contains('Instructions Sent!').should('be.visible');
 
