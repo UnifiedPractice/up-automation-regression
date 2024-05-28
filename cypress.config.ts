62ca507be6e2f4112eca629b511cb89a9790e934
alexandrucristian15 <87300455+alexandrucristian15@users.noreply.github.com>
2024-05-28 16:04:16 +0300

    Patient Portal Regression - 28.05.2024 - 2

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,8 +5,8 @@ export default defineConfig({
   projectId: "hqze8x",
   pageLoadTimeout: 140000,
   retries: {
-    runMode: 500,
-    openMode: 500
+    runMode: 8,
+    openMode: 8
   },
   e2e: {
     // We've imported your old cypress plugins here.
